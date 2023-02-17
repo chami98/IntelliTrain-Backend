806d0c3009bb7698c5eea13127fcb779693e0e71
chami98 <chamikara98@gmail.com>
2023-02-17 19:02:25 +0530

    Created post method to add data to the firestore

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 const serviceAccount = require('./serviceAccount.json');
+const bodyParser = require('body-parser');
+
 
 initializeApp({
     credential: cert(serviceAccount)
@@ -11,6 +13,7 @@ initializeApp({
 const db = getFirestore();
 // Create a new Express app
 const app = express();
+app.use(bodyParser.json());
 
 // Define a route to fetch data from Firestore
 app.get('/data', async (req, res) => {
@@ -21,6 +24,18 @@ app.get('/data', async (req, res) => {
             data.push(doc.data());
         });
         res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.post('/data', async (req, res) => {
+    try {
+        console.log(req.body)
+        // const data = req.body;
+        // const docRef = await db.collection('users').add(data);
+        // res.status(201).json({ id: docRef.id });
     } catch (error) {
         console.log(error);
         res.status(500).send(error);
